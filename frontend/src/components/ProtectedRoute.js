49f25abba31a8202752fa782ac0255e096c9f524
Geometr <geo.fsv.p@gmail.com>
2023-08-19 00:40:20 +0300

    feat: Скрыта папка frontend

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,5 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-export default function ProtectedRoute({ element: Component, isLoggedIn, ...props }) {
-  return isLoggedIn ? <Component {...props} /> : <Navigate to="/sign-in" replace />;
-}
