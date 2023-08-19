c276186110357b1af6dcaf3ef0a1588eee477397
Geometr <geo.fsv.p@gmail.com>
2023-08-19 12:40:22 +0300

    feat: Добавлен роут краштеста

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,12 @@ app.use(cookieParser());
 app.use(cors({ origin: FRONT_URL, credentials: true }));
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post(
   '/signin',
   celebrate({
